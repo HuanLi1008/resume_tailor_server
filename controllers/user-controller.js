beb649c4b70b1dd8ba3d443cbcf8c55f05063df5
Huan Li <huanli01008@gmail.com>
2024-03-23 23:11:08 -0400

    fix bug, post user controller should return created user object instead of an array

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,7 +7,7 @@ const postuser = async(req, res)=>{
     try {
         const insert = await knex("user").insert(req.body);
         const inserted_user = await knex("user").where({"id": insert[0]});
-        return res.status(201).json(inserted_user);
+        return res.status(201).json(inserted_user[0]);
     } catch (error) {
         return res.status(500).json({error: {message: `Can't post user ${error}`}});
     }
