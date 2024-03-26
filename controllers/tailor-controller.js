09a7ccc0c84f1981fb45f5940c9819d6ebd44143
Huan Li <huanli01008@gmail.com>
2024-03-26 15:39:46 -0400

    set up nlp api and get keywords from nlpcloud api

--- controllers/tailor-controller.js
+++ controllers/tailor-controller.js
@@ -1,5 +1,39 @@
-const tailorresume = (req, res)=>{
+const knex = require("knex")(require("../knexfile"));
+require("dotenv").config();
+
+const NLPCloudClient = require('nlpcloud');
+
+
+const tailorResume = async (req, res)=>{
+    try {
+        const user_id = req.params.userid;
+        const foundResume = await knex("resume")
+            .where("user_id", user_id);
+        let resume = foundResume[0];
+        if(!resume){
+            return res.json({message: "no resume"});
+        }
+        const {jd}=req.body;
+        // const keywords = await getKeywords(jd);
+        const keywords = ["Bachelor's degree","Computer Science","Engineering","Full Stack Developer","PHP","Laravel framework","HTML","CSS","JavaScript","Vue.js","MySQL","Git","GitLab","Docker","software development lifecycle","Agile methodologies","problem-solving","communication skills"];
+        
+        
+    } catch (error) {
+        
+    }
     res.send('This is tailor page')
 }
 
-module.exports = {tailorresume};
+async function getKeywords(jd){
+    const client = new NLPCloudClient({model:'dolphin',token:process.env.TOKEN,gpu:true});
+    try {
+        const message = jd.replaceAll("\n", " ");
+        
+        const response = await client.kwKpExtraction({text:message});
+        return response.data["keywords_and_keyphrases"];
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = {tailorResume};
