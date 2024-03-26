0ae165f297ae0b0feb52fe0cfbbe408a68795259
Huan Li <huanli01008@gmail.com>
2024-03-26 16:19:51 -0400

    calculate score based on keywords

--- controllers/tailor-controller.js
+++ controllers/tailor-controller.js
@@ -13,17 +13,33 @@ const tailorResume = async (req, res)=>{
         if(!resume){
             return res.json({message: "no resume"});
         }
+        // get full resume
+        const resume_id = resume.id;
+        const linkrows = await knex("link").where("resume_id", resume_id);
+        const educationrows = await knex("education").where("resume_id", resume_id);
+        const experiencerows = await knex("experience").where("resume_id", resume_id);
+        const projectrows = await knex("project").where("resume_id", resume_id);
+        // get keywords array
         const {jd}=req.body;
         // const keywords = await getKeywords(jd);
         const keywords = ["Bachelor's degree","Computer Science","Engineering","Full Stack Developer","PHP","Laravel framework","HTML","CSS","JavaScript","Vue.js","MySQL","Git","GitLab","Docker","software development lifecycle","Agile methodologies","problem-solving","communication skills"];
-        
-        
-    } catch (error) {
-        
-    }
-    res.send('This is tailor page')
-}
 
+        // tailor experience
+        const test = "Utilized React, HTML, Css, and Sass to create a responsive front-end, enhancing user experience across all screen sizes."
+        return res.json(calcScore(test, keywords));
+
+    } catch (error) {
+        return res.status(500).json({error: {message:`Can not tailor resume: ${error}`}})
+    }
+    
+}
+function calcScore(sentence, keywords){
+    let score = 0;
+    for(const word of keywords){
+        if(new RegExp(word,'i').test(sentence)) score++;
+    }
+    return score;
+}
 async function getKeywords(jd){
     const client = new NLPCloudClient({model:'dolphin',token:process.env.TOKEN,gpu:true});
     try {
