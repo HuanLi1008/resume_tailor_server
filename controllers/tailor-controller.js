426e7efce78576652588fef6ade71de91bc2aca5
Huan Li <huanli01008@gmail.com>
2024-03-26 21:04:55 -0400

    add tailorObject helper function to tailor object array according to scores calculated by calcscore

--- controllers/tailor-controller.js
+++ controllers/tailor-controller.js
@@ -25,8 +25,8 @@ const tailorResume = async (req, res)=>{
         const keywords = ["Bachelor's degree","Computer Science","Engineering","Full Stack Developer","PHP","Laravel framework","HTML","CSS","JavaScript","Vue.js","MySQL","Git","GitLab","Docker","software development lifecycle","Agile methodologies","problem-solving","communication skills"];
 
         // tailor experience
-        const test = "Utilized React, HTML, Css, and Sass to create a responsive front-end, enhancing user experience across all screen sizes."
-        return res.json(calcScore(test, keywords));
+         
+        return res.json(tailorObject(experiencerows, keywords));
 
     } catch (error) {
         return res.status(500).json({error: {message:`Can not tailor resume: ${error}`}})
@@ -39,6 +39,39 @@ function calcScore(sentence, keywords){
         if(new RegExp(word,'i').test(sentence)) score++;
     }
     return score;
+}
+function tailorObject(arr, keywords){
+    let map = []
+    arr.forEach((cur, i)=>{
+        
+        const bullet_points=cur.bullet_points;
+        const pointsArr = bullet_points.split("\\n");
+        const pointsMap = [];
+        for(const point of pointsArr){
+            const score = calcScore(point, keywords);
+            pointsMap.push({point: point, score: score});
+        }
+        
+        let objScore = 0;
+        if(pointsArr.length > 3){
+            pointsMap.sort((a,b)=> b.score - a.score);
+            arr[i].bullet_points = pointsMap.slice(0, 3).map((item) => item.point).join("\\n");
+            objScore = pointsMap.slice(0, 3).reduce((a, c) => a + c.score, 0);
+            
+            
+        }else{
+            objScore = pointsMap.reduce((a, c)=> a + c.score, 0);
+        
+        }
+        map.push({obj: arr[i], score: objScore});
+        
+        
+    })
+    
+    map.sort((a, b)=> b.score - a.score);
+    
+    if(arr.length > 3) map = map.slice(0,3);
+    return map.map((i) => i.obj);
 }
 async function getKeywords(jd){
     const client = new NLPCloudClient({model:'dolphin',token:process.env.TOKEN,gpu:true});
