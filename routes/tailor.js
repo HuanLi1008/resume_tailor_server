db699428741b5695e49fe156345875407f6a9856
Huan Li <huanli01008@gmail.com>
2024-03-27 16:59:55 -0400

    fix bug, change get to post to handle post jd of tailor endpoint

--- routes/tailor.js
+++ routes/tailor.js
@@ -3,7 +3,7 @@ const tailorController = require('../controllers/tailor-controller');
 const {tailorValidator} = require("../middlewares/tailor-validator");
 const {useridValidator} = require("../middlewares/resume-validator");
 router.route('/:userid')
-    .get([useridValidator, tailorValidator], tailorController.tailorResume)
+    .post([useridValidator, tailorValidator], tailorController.tailorResume)
 
 
 module.exports = router;
