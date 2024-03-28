af88f8082330e7849e85a194456a1d493c5bdd6a
Huan Li <huanli01008@gmail.com>
2024-03-27 22:05:44 -0400

    fix bug, change split and join paramter

--- controllers/tailor-controller.js
+++ controllers/tailor-controller.js
@@ -21,8 +21,8 @@ const tailorResume = async (req, res)=>{
         const projectrows = await knex("project").where("resume_id", resume_id);
         // get keywords array
         const {jd}=req.body;
-        // const keywords = await getKeywords(jd);
-        const keywords = ["Bachelor's degree","Computer Science","Engineering","Full Stack Developer","PHP","Laravel framework","HTML","CSS","JavaScript","Vue.js","MySQL","Git","GitLab","Docker","software development lifecycle","Agile methodologies","problem-solving","communication skills"];
+        const keywords = await getKeywords(jd);
+        // const keywords = ["Bachelor's degree","Computer Science","Engineering","Full Stack Developer","PHP","Laravel framework","HTML","CSS","JavaScript","Vue.js","MySQL","Git","GitLab","Docker","software development lifecycle","Agile methodologies","problem-solving","communication skills"];
 
         // tailor experience and project
         const tailoredExperience = tailorObject(experiencerows, keywords);
@@ -48,7 +48,7 @@ function tailorObject(arr, keywords){
     arr.forEach((cur, i)=>{
         
         const bullet_points=cur.bullet_points;
-        const pointsArr = bullet_points.split("\\n");
+        const pointsArr = bullet_points.split("\n");
         const pointsMap = [];
         for(const point of pointsArr){
             const score = calcScore(point, keywords);
@@ -58,7 +58,7 @@ function tailorObject(arr, keywords){
         let objScore = 0;
         if(pointsArr.length > 3){
             pointsMap.sort((a,b)=> b.score - a.score);
-            arr[i].bullet_points = pointsMap.slice(0, 3).map((item) => item.point).join("\\n");
+            arr[i].bullet_points = pointsMap.slice(0, 3).map((item) => item.point).join("\n");
             objScore = pointsMap.slice(0, 3).reduce((a, c) => a + c.score, 0);
             
             
